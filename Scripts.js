997fd087e12efd77fd2317d202f0dd691fabfc99
ljk1212 <lemankaifa@gmail.com>
2024-05-20 17:38:34 -0400

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,22 @@
+var map;
+var service;
+var infowindow;
+var userLocation;
+var autocomplete;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+  infowindow = new google.maps.InfoWindow();
+  autocomplete = new google.maps.places.Autocomplete(document.getElementById('address'));
+  autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged() {
+  // Your onPlaceChanged function code here
+}
+
+// Add the rest of your JavaScript functions here
 
